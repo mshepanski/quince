315f4690643d43494ac5c73c05910312a432608f
Michael Shepanski <mps@optusnet.com.au>
2015-03-06 14:15:00 +1100

    Change licence from GPL 3.0 to Boost Software License 1.0

--- include/quince/mappers/direct_mapper.h
+++ include/quince/mappers/direct_mapper.h
@@ -1,24 +1,10 @@
 #ifndef QUINCE__mappers__direct_mapper_h
 #define QUINCE__mappers__direct_mapper_h
 
-/*
-    Copyright 2014 Michael Shepanski
-
-    This file is part of the quince library.
-
-    Quince is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Quince is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with quince.  If not, see <http://www.gnu.org/licenses/>.
-*/
+//          Copyright Michael Shepanski 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <quince/detail/compiler_specific.h>
 #include <quince/detail/row.h>
