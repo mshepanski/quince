77931b643b373db3ec954ae539a639b9421ce40a
Jonny Weir <jonny@jweir.co.uk>
2017-07-14 16:13:39 +0100

    Update Copyright header

--- include/quince/detail/time_type.h
+++ include/quince/detail/time_type.h
@@ -1,7 +1,7 @@
 #ifndef QUINCE__mappers__detail__time_type_h
 #define QUINCE__mappers__detail__time_type_h
 
-//          Copyright Michael Shepanski 2015.
+//          Copyright Jonny Weir 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file ../../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
